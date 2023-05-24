3732fa97b67d419445bef5507df0ddf6d712fdc0
wighs33 <55389203+wighs33@users.noreply.github.com>
2023-05-24 17:54:23 +0900

    프로젝트 생성

---
+++ Previous_Coding_Test/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+template<typename T>
+void ee(T t) { cout << t << '\n'; }
+template<typename T>
+void ss(T t) { cout << t << ' '; }
+
+
+
+int main()
+{
+	ee("a");
+	ee("b");
+	//for (int d : solution())
+	//	ss(d);
+	//ee("");
+}
